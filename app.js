b0e2054ca7b24345f2b2b179b94a0d4313b6326c
MDIVS <maiconoficialbr@gmail.com>
2024-09-07 13:54:39 -0300

    enable swagger documentation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Postgres = require('./src/db/postgres');
 const init_schema_user = require('./src/db/schemas/user');
 
 const Hapi = require('@hapi/hapi');
+const Vision = require('@hapi/vision');
+const Inert = require('@hapi/inert');
+const Swagger = require('hapi-swagger');
 const RouteUser = require('./src/routes/user');
 
 async function main() {
@@ -19,16 +22,32 @@ async function main() {
 
     server.route(
         [
-            {
-                method: 'GET',
-                path: '/',
-                handler: (request, h) => {
-                    return 'Hello World!';
-                }
-            },
             ...new RouteUser(connection).routes
         ]
     );
+
+    const swaggerOptions = {
+        documentationPath: '/',
+        info: {
+            title: `Node Js User Handler (${process.env.ENV_NAME})`,
+            version: require('./package.json').version,
+            description: 'Open source backend for handling user sign in and sign out with Node js and PostgreSQL.',
+            contact: {
+                name: 'MDIVS',
+                email: 'maiconoficialbr@gmail.com',
+                url: 'https://github.com/MDIVS/nodejs-user-handler'
+            }
+        }
+    };
+
+    await server.register([
+        Vision,
+        Inert,
+        {
+            plugin: Swagger,
+            options: swaggerOptions
+        }
+    ]);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 class UserRoutes {
     /**
      * @param {Sequelize} db sequelize connection to database
@@ -11,16 +13,33 @@ class UserRoutes {
             path: '/user',
             method: 'POST',
             options: {
+                tags: ['api'],
                 description: 'Creates user in database',
-                handler: async (request) => {
-                    const { record } = request.payload;
-                    let new_record = await this.db.models.User.create(record);
-                    
-                    return {
-                        message: 'Record successfully commited to database.',
-                        record: new_record
-                    };
-                }
+                validate: {
+                    payload: Joi.object({
+                        record: {
+                            username:                   Joi.string().min(5).max(100).required(),
+                            firstname:                  Joi.string().min(3).max(100).required(),
+                            middlename:                 Joi.string().min(3).max(100),
+                            lastname:                   Joi.string().min(3).max(100).required(),
+                            fullname:                   Joi.string().min(3).max(255).required(),
+                            preferredname:              Joi.string().min(3).max(32),
+
+                            email:                      Joi.string().email({minDomainSegments: 1}).required(),
+                            phone:                      Joi.string().min(8).max(13).allow(null,''),
+                            password:                   Joi.string().optional().min(8).max(16).required()
+                        }
+                    })
+                },
+            },
+            handler: async (request) => {
+                const { record } = request.payload;
+                let new_record = await this.db.models.User.create(record);
+                
+                return {
+                    message: 'Record successfully commited to database.',
+                    record: new_record
+                };
             }
         }
     ];
